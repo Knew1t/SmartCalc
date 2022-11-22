45059df62d988d16e0911c50e43940c6bb38baf8
Knew1t <longshot@oa-a3.msk.21-school.ru>
2022-11-22 19:43:14 +0300

    button output

--- src/qt/SmartCalc/mainwindow.cpp
+++ src/qt/SmartCalc/mainwindow.cpp
@@ -2,40 +2,121 @@
 #include "./ui_mainwindow.h"
 //#include "QtCore/qobjectdefs.h"
 
-//char math_expression[255] = "";
+// char math_expression[255] = "";
 bool addTrigger = false;
+bool divTrigger = false;
+bool multTrigger = false;
+bool digitTrigger = false;
+bool dotTrigger = false;
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
   setFixedSize(geometry().width(), geometry().height());
-//  ui->Display->setReadOnly(true);
+  //  ui->Display->setReadOnly(true);
   ui->Display->setText("");
   ui->Display->setAlignment(Qt::AlignRight);
   QPushButton *digitButtons[10];
-  for (int i = 0; i < 10; i++){
-     QString buttonName = "button_" + QString::number(i);
-     digitButtons[i] = MainWindow::findChild<QPushButton *>(buttonName);
-     connect(digitButtons[i], SIGNAL (released()),this,  SLOT (digitPressed())) ;
+  for (int i = 0; i < 10; i++) {
+    QString buttonName = "button_" + QString::number(i);
+    digitButtons[i] = MainWindow::findChild<QPushButton *>(buttonName);
+    connect(digitButtons[i], SIGNAL(released()), this, SLOT(digitPressed()));
   }
-  connect(ui->button_clear, SIGNAL (released()), this, SLOT (clearPressed()));
+  connect(ui->button_clear, SIGNAL(released()), this, SLOT(clearPressed()));
+  connect(ui->button_plus, SIGNAL(released()), this, SLOT(plusPressed()));
+  connect(ui->button_minus, SIGNAL(released()), this, SLOT(minusPressed()));
+  connect(ui->button_open_parent, SIGNAL(released()), this,
+          SLOT(openParentPressed()));
+  connect(ui->button_close_parent, SIGNAL(released()), this,
+          SLOT(closeParentPressed()));
 
+  QPushButton *trigonometryButtons[8] = {
+      ui->button_sin,  ui->button_cos,  ui->button_tg, ui->button_acos,
+      ui->button_asin, ui->button_atan, ui->button_ln, ui->button_log};
+  for (int i = 0; i < 8; i++) {
+    connect(trigonometryButtons[i], SIGNAL(released()), this,
+            SLOT(trigonometryButtonPressed()));
+  }
+
+  connect(ui->button_multiply, SIGNAL(released()), this,
+          SLOT(multiplyPressed()));
+  connect(ui->button_divide, SIGNAL(released()), this, SLOT(dividePressed()));
+  connect(ui->button_equal, SIGNAL(released()), this, SLOT(equalPressed()));
+  connect(ui->button_dot, SIGNAL(released()), this, SLOT(dotPressed()));
 }
 
 MainWindow::~MainWindow() { delete ui; }
 
-void MainWindow::clearPressed(){
+void MainWindow::trigonometryButtonPressed() {
+  QPushButton *button = (QPushButton *)sender();
+  QString outputString = button->text() + "(";
+
+  ui->Display->insertPlainText(outputString);
+}
+
+void MainWindow::clearPressed() {
   ui->Display->setText("");
   ui->Display->setAlignment(Qt::AlignRight);
-
-}
-void MainWindow::digitPressed(){
-   QPushButton *button = (QPushButton *)sender();
-   QString buttonValue = button->text();
-   //QString displayVal = ui->Display->placeholderText();
-   //QString newVal = displayVal + butval;
-   ui->Display->insertPlainText(buttonValue);
+  addTrigger = false;
+  divTrigger = false;
+  multTrigger = false;
+  digitTrigger = false;
+  dotTrigger = false;
 }
 
+void MainWindow::digitPressed() {
+  QPushButton *button = (QPushButton *)sender();
+  QString buttonValue = button->text();
+  // QString displayVal = ui->Display->placeholderText();
+  // QString newVal = displayVal + butval;
+  ui->Display->insertPlainText(buttonValue);
+  digitTrigger = true;
+  multTrigger = false;
+  divTrigger = false;
+  dotTrigger = false;
+}
+void MainWindow::openParentPressed() {
+  if (!dotTrigger) {
+    ui->Display->insertPlainText("(");
+    dotTrigger = true;
+  }
+}
+void MainWindow::closeParentPressed() {
+  if (!dotTrigger)
+    ui->Display->insertPlainText(")");
+  dotTrigger = true;
+}
+void MainWindow::dotPressed() {
+  if (!dotTrigger) {
+    ui->Display->insertPlainText(".");
+    dotTrigger = true;
+  }
+}
+// ============== OPERATIONS =======================
+void MainWindow::plusPressed() { ui->Display->insertPlainText("+"); }
 
+void MainWindow::minusPressed() {
+  ui->Display->insertPlainText("-");
+  dotTrigger = true;
+}
 
-
+void MainWindow::multiplyPressed() {
+  if (!multTrigger && !divTrigger) {
+    ui->Display->insertPlainText("*");
+    multTrigger = true;
+    divTrigger = true;
+  }
+}
+void MainWindow::dividePressed() {
+  if (!multTrigger && !divTrigger) {
+    ui->Display->insertPlainText("/");
+    multTrigger = true;
+    divTrigger = true;
+  }
+}
+//==================================================
+void MainWindow::equalPressed() {
+  QString mathExpression = ui->Display->toPlainText();
+  ui->Display->setText(mathExpression);
+  ui->Display->setAlignment(Qt::AlignRight);
+  ui->Display->moveCursor(QTextCursor::End);
+}
--- src/qt/SmartCalc/mainwindow.h
+++ src/qt/SmartCalc/mainwindow.h
@@ -6,21 +6,32 @@
 #include <QToolButton>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+class MainWindow;
+}
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+  MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
 
 private slots:
-    void digitPressed();
-    void clearPressed();
+  void digitPressed();
+  void clearPressed();
+  void plusPressed();
+  void minusPressed();
+  void multiplyPressed();
+  void dividePressed();
+  void equalPressed();
+  void openParentPressed();
+  void closeParentPressed();
+  void dotPressed();
+  void trigonometryButtonPressed();
+
 private:
-    Ui::MainWindow *ui;
+  Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
