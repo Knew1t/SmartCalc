c9680335879fba3705c86908e892895626d1de2b
Knew1t <longshot@oa-a3.msk.21-school.ru>
2022-11-24 17:48:52 +0300

    checkpoint

--- src/qt/SmartCalc/backend.c
+++ src/qt/SmartCalc/backend.c
@@ -5,6 +5,46 @@ int Calculate(char input_string[255]) {
   return 0;
 }
 
-int ParseMathExpression(char input_string[255]) {}
-return 0;
+int ParseMathExpression(char input_string[255]) {
+  char *lexeme = calloc(1, sizeof(char));
+  CheckIfAllocationFailed(lexeme);
+  for (char *letter_pointer = input_string; *letter_pointer != '\0';
+       letter_pointer++) {
+    if (IsDigit(letter_pointer)) {
+      int count_digits = 0;
+      do {
+        ++count_digits;
+        ++letter_pointer;
+      } while (IsDigit(letter_pointer));
+      lexeme = realloc(lexeme, count_digits + 1);
+      CheckIfAllocationFailed(lexeme);
+      memcpy(lexeme, letter_pointer, sizeof(char) * count_digits);
+    }
+    if (IsLetter(letter_pointer)) {
+    }
+    if (IsSign(letter_pointer)) {
+    }
+  }
+  return 0;
+}
+
+int CheckIfAocationFailed(void *ptr) {
+  if (ptr == NULL)
+    exit(FAILURE);
+  return 0;
+}
+
+bool IsDigit(char const *letter_pointer) {
+  return (*letter_pointer >= 48 && *letter_pointer <= 57) ||
+         *letter_pointer == '.';
+}
+
+bool IsLetter(char const *letter_pointer) {
+  return (*letter_pointer >= 65 && *letter_pointer <= 90) ||
+         (*letter_pointer >= 97 && *letter_pointer <= 122);
+}
+
+bool IsSign(char const *letter_pointer) {
+  return *letter_pointer == '+' || *letter_pointer == '-' ||
+         *letter_pointer == '*' || *letter_pointer == '/';
 }
--- src/qt/SmartCalc/backend.h
+++ src/qt/SmartCalc/backend.h
@@ -1,20 +1,32 @@
 #ifndef BACKEND_H
 #define BACKEND_H
 
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 typedef struct ConversionStackLexemes {
   char *lexeme;
+
   ConversionStackLexemes *link;
 } ConversionStackLexemes;
 
 typedef struct ReversedPolishNotationLine {
   char *lexeme;
+
   ReversedPolishNotationLine *link;
 } ReversedPolishNotationLine;
 
+enum { OK, FAILURE };
+
 int Calculate(char string[255]);
 int ParseMathExpression(char string[255]);
+int ToStack(char *lexeme);
+int ToLine(char *lexeme);
+bool CheckIfAllocationFailed(void *ptr);
+bool IsDigit(char const *letter_pointer);
+bool IsLetter(char const *lexeme);
+bool IsSign(char const *lexeme);
 
 #endif /* ifndef BACKEND_H */
