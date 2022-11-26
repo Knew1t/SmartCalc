ceeac148228225e54687e1c971553acb415db5b4
Knew1t <longshot@ox-h6.msk.21-school.ru>
2022-11-26 19:44:32 +0300

    nov 26 logout

--- src/qt/SmartCalc/backend.c
+++ src/qt/SmartCalc/backend.c
@@ -7,43 +7,76 @@ int Calculate(char input_string[255]) {
     LexemeList *rpn_line_head = NULL;
     CreateLinkedList(&rpn_line_head);
     ParseMathExpression(rpn_line_head, input_string);
-    PrintRPNLine(rpn_line_head);
+    /* PrintRPNList(rpn_line_head); */
     DeleteLinkedList(rpn_line_head);
   }
   return 0;
 }
 
-int ParseMathExpression(LexemeList *head, char input_string[255]) {
+int ParseMathExpression(LexemeList *rpn_line_head, char input_string[255]) {
   if (strlen(input_string) == 0)
     return OK;
 
+  LexemeList *stack_head = NULL;
+  CreateLinkedList(&stack_head);
+
   for (char *letter_pointer = input_string; *letter_pointer != '\0';
        letter_pointer++) {
-    /* LexemeList *stack_head = NULL; */
-    /* CreateLinkedList(stack_head); */
     if (IsDigit(letter_pointer)) {
       char *lexeme = NULL;
       GetNumberLexeme(&lexeme, &letter_pointer);
-      ToLine(head, lexeme);
+      ToRPNQue(rpn_line_head, lexeme);
       free(lexeme);
     }
     if (IsLetter(letter_pointer)) {
     }
-    if (IsSign(letter_pointer)) {
+    if (IsOperator(letter_pointer)) {
+      char operator= *(letter_pointer);
+      if (stack_head->lexeme) {
+        while (CompareToStackOperator(stack_head, operator)) {
+          ToRPNQue(rpn_line_head, stack_head->lexeme);
+          DeleteHeadNode(&stack_head);
+        };
+      }
+      ToStack(&stack_head, &operator);
     }
   }
+  DeleteLinkedList(stack_head);
   return OK;
 }
 
+int CompareToStackOperator(LexemeList *head, char operator) {
+  int priority = 0;
+  int stack_operator_priority = 0;
+  AssignPriority(&priority, &operator);
+  AssignPriority(&stack_operator_priority, head->lexeme);
+  return priority < stack_operator_priority;
+}
+
+void AssignPriority(int *priority, const char *operator) {
+  if (*operator== '-') {
+    *priority = 1;
+  }
+  if (*operator== '+') {
+    *priority = 2;
+  }
+  if (*operator== '/') {
+    *priority = 3;
+  }
+  if (*operator== '*') {
+    *priority = 4;
+  }
+}
 int CreateLinkedList(LexemeList **head) {
   LexemeList *first_node = calloc(1, sizeof(LexemeList));
   CheckIfAllocationFailed(first_node);
   first_node->link = NULL;
+  first_node->lexeme = NULL;
   *head = first_node;
   return OK;
 }
 
-int ToLine(LexemeList *head, char *incoming_lexeme) {
+int ToRPNQue(LexemeList *head, char *incoming_lexeme) {
   LexemeList *tmp = head;
   while (tmp->link != NULL) {
     tmp = tmp->link;
@@ -55,10 +88,25 @@ int ToLine(LexemeList *head, char *incoming_lexeme) {
   return OK;
 }
 
+int ToStack(LexemeList **head, char *incoming_lexeme) {
+  LexemeList *tmp = calloc(1, sizeof(incoming_lexeme));
+  strcpy(tmp->lexeme, incoming_lexeme);
+  tmp->link = (*head);
+  *head = tmp;
+  return 0;
+}
+
 int AddNodeAtTheEnd(LexemeList **node) {
   *node = calloc(1, sizeof(LexemeList));
   CheckIfAllocationFailed(*node);
   (*node)->link = NULL;
+  return 0;
+}
+
+int DeleteHeadNode(LexemeList **head) {
+  LexemeList *new_head = (*head)->link;
+  free(*head);
+  *head = new_head;
   return 0;
 }
 
@@ -67,10 +115,9 @@ int DeleteLinkedList(LexemeList *head) {
   while (tmp != NULL) {
     tmp = tmp->link;
     free(head);
+    free(head->lexeme);
     head = tmp;
   }
-
-  //
   return 0;
 }
 
@@ -111,7 +158,7 @@ bool IsLetter(char const *letter_pointer) {
          (*letter_pointer >= 97 && *letter_pointer <= 122);
 }
 
-bool IsSign(char const *letter_pointer) {
+bool IsOperator(char const *letter_pointer) {
   return *letter_pointer == '+' || *letter_pointer == '-' ||
          *letter_pointer == '*' || *letter_pointer == '/';
 }
--- src/qt/SmartCalc/backend.h
+++ src/qt/SmartCalc/backend.h
@@ -16,18 +16,21 @@ enum { OK, FAILURE };
 int Calculate(char string[255]);
 int ParseMathExpression(LexemeList *head, char string[255]);
 int GetNumberLexeme(char **lexeme, char **letter_pointer);
+int CompareToStackOperator(LexemeList *head, char operato);
+void AssignPriority(int *priority, const char *operato);
 
 int CreateLinkedList(LexemeList **head);
 int DeleteLinkedList(LexemeList *head);
 int AddNodeAtTheEnd(LexemeList **current_node);
-int ToStack(char *lexeme);
-int ToLine(LexemeList *head, char *lexeme);
+int ToRPNQue(LexemeList *head, char *lexeme);
+int ToStack(LexemeList **head, char *lexeme);
+int DeleteHeadNode(LexemeList **head);
 void PrintRPNLine(LexemeList *rpn_line_head);
 
 bool IsInputCorrect(char input_string[]);
 bool CheckIfAllocationFailed(void *ptr);
 bool IsDigit(char const *letter_pointer);
 bool IsLetter(char const *lexeme);
-bool IsSign(char const *lexeme);
+bool IsOperator(char const *lexeme);
 
 #endif /* ifndef BACKEND_H */
