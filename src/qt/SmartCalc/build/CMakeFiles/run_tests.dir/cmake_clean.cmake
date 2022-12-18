4b60ef60f14973f7027653dc13ea6ba24e0dcd5c
Knew1t <longshot@oa-a3.msk.21-school.ru>
2022-12-18 17:52:33 +0300

    done

---
+++ src/qt/SmartCalc/build/CMakeFiles/run_tests.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run_tests_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/run_tests_autogen.dir/ParseCache.txt"
+  "run_tests_autogen"
+  "CMakeFiles/run_tests.dir/backend.c.o"
+  "CMakeFiles/run_tests.dir/backend.c.o.d"
+  "CMakeFiles/run_tests.dir/run_tests_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/run_tests.dir/run_tests_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/run_tests.dir/tests/test_main.c.o"
+  "CMakeFiles/run_tests.dir/tests/test_main.c.o.d"
+  "run_tests"
+  "run_tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/run_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/qt/SmartCalc/build/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/longshot/projects/SmartCalc/src/qt/SmartCalc/backend.c" "CMakeFiles/test.dir/backend.c.o" "gcc" "CMakeFiles/test.dir/backend.c.o.d"
+  "/Users/longshot/projects/SmartCalc/src/qt/SmartCalc/tests/test_main.c" "CMakeFiles/test.dir/tests/test_main.c.o" "gcc" "CMakeFiles/test.dir/tests/test_main.c.o.d"
+  "/Users/longshot/projects/SmartCalc/src/qt/SmartCalc/build/test_autogen/mocs_compilation.cpp" "CMakeFiles/test.dir/test_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/test.dir/test_autogen/mocs_compilation.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
